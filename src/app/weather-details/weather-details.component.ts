06c42e3be0c677fbaea7c972baa4d72ba7ae5877
will-lp <will_lp@ymail.com>
2019-03-19 14:22:16 -0300

    Adicionada integração de 'details' com owm. adicionado momentjs para interpretar e formatar a data.

--- src/app/weather-details/weather-details.component.ts
+++ src/app/weather-details/weather-details.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { OpenWeatherMapService } from '../open-weather-map.service';
 
+import * as moment from 'moment';
+
 const ENTER_KEYCODE = 13;
 
 @Component({
@@ -25,12 +27,33 @@ export class WeatherDetailsComponent implements OnInit {
   ngOnInit() {
     this.cityId = this.route.snapshot.params.id;
     this.openWeatherMapService.currentWeather(this.cityId).subscribe(result => {
-      this.cityWeather = result;
+      this.cityWeather = this.addSunsetAndSunriseDate(result);
       this.searchText = this.cityWeather.name;
     });
       
     this.openWeatherMapService.forecast(this.cityId)
-      .subscribe(result => this.cityForecast = result);
+      .subscribe(result => {
+        this.cityForecast = this.addForecastDatetime(result);
+      });
+  }
+  
+  addSunsetAndSunriseDate(result) {
+    result.sys['sunrise_dt'] = new Date(result.sys.sunrise);
+    result.sys['sunset_dt'] = new Date(result.sys.sunset);
+    return result;
+  }
+  
+  addForecastDatetime(result) {
+    result.list.forEach(forecast => {
+      const dt_txt = forecast.dt_txt.split(' ');
+      forecast['dt_day'] = this.toDate(dt_txt[0]);
+      forecast['dt_hour'] = dt_txt[1];
+    });
+    return result;
+  }
+  
+  toDate(dateTxt) {
+    return moment(dateTxt).format('ddd D MMM');
   }
   
   searchOnEnter(event) {
